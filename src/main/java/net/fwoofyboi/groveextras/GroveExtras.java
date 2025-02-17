792460b3e6232ea09101437fedfb9cd7f2b04019
FwoofyBoi <alexahicks18@gmail.com>
2025-02-17 14:41:50 -0600

    Initial commit

---
+++ src/main/java/net/fwoofyboi/groveextras/GroveExtras.java
@@ -0,0 +1,16 @@
+package net.fwoofyboi.groveextras;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GroveExtras implements ModInitializer {
+	public static final String MOD_ID = "groveextras";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/fwoofyboi/groveextras/GroveExtrasClient.java
@@ -0,0 +1,10 @@
+package net.fwoofyboi.groveextras;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GroveExtrasClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
