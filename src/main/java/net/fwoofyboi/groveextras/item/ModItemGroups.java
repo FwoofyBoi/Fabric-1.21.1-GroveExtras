c608dcc639c0887f0f436508e7fdb0c19e21825a
FwoofyBoi <alexahicks18@gmail.com>
2025-02-19 18:43:28 -0600

    Implemented tab for Treasures, added non crystal shard treasure items

--- src/main/java/net/fwoofyboi/groveextras/item/ModItemGroups.java
+++ src/main/java/net/fwoofyboi/groveextras/item/ModItemGroups.java
@@ -21,6 +21,32 @@ public class ModItemGroups {
                         entries.add(ModItems.KEY_COSMETIC);
                     }).build());
 
+    public static final ItemGroup TREASURE_ITEMS_GROUP = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(GroveExtras.MOD_ID, "treasure_items"),
+            FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.RUBY))
+                    .displayName(Text.translatable("itemgroup.groveextras.treasure_items"))
+                    .entries((displayContext, entries) -> {
+                        entries.add(ModItems.TOPAZ);
+                        entries.add(ModItems.CUT_DIAMOND);
+                        entries.add(ModItems.SAPPHIRE);
+                        entries.add(ModItems.RUBY);
+                        entries.add(ModItems.PEARL);
+                        entries.add(ModItems.CRYSTALLIZED_AMBER);
+                        entries.add(ModItems.CUT_AMETHYST);
+                        entries.add(ModItems.NUGGET);
+                        entries.add(ModItems.BIG_PEARL);
+                        entries.add(ModItems.BIG_NUGGET);
+                        entries.add(ModItems.COMET_SHARD);
+                        entries.add(ModItems.STAR_PIECE);
+                        entries.add(ModItems.STAR_DUST);
+                        entries.add(ModItems.WITHER_SKULL_FRAGMENT);
+                        entries.add(ModItems.RED_SHARD);
+                        entries.add(ModItems.BLUE_SHARD);
+                        entries.add(ModItems.GREEN_SHARD);
+                        entries.add(ModItems.YELLOW_SHARD);
+                        entries.add(ModItems.PRETTY_FEATHER);
+                    }).build());
+
     public static void registerItemGroups() {
         GroveExtras.LOGGER.info("Registering Item Groups for " + GroveExtras.MOD_ID);
     }
--- src/main/java/net/fwoofyboi/groveextras/item/ModItems.java
+++ src/main/java/net/fwoofyboi/groveextras/item/ModItems.java
@@ -14,6 +14,25 @@ public class ModItems {
     public static final Item KEY_MASTER = registerItem("key_master", new Item(new Item.Settings()));
     public static final Item KEY_COSMETIC = registerItem("key_cosmetic", new Item(new Item.Settings()));
 
+    public static final Item TOPAZ = registerItem("topaz", new Item(new Item.Settings()));
+    public static final Item CUT_DIAMOND = registerItem("cut_diamond", new Item(new Item.Settings()));
+    public static final Item SAPPHIRE = registerItem("sapphire", new Item(new Item.Settings()));
+    public static final Item RUBY = registerItem("ruby", new Item(new Item.Settings()));
+    public static final Item PEARL = registerItem("pearl", new Item(new Item.Settings()));
+    public static final Item CRYSTALLIZED_AMBER = registerItem("crystallized_amber", new Item(new Item.Settings()));
+    public static final Item CUT_AMETHYST = registerItem("cut_amethyst", new Item(new Item.Settings()));
+    public static final Item NUGGET = registerItem("nugget", new Item(new Item.Settings()));
+    public static final Item BIG_PEARL = registerItem("big_pearl", new Item(new Item.Settings()));
+    public static final Item BIG_NUGGET = registerItem("big_nugget", new Item(new Item.Settings()));
+    public static final Item COMET_SHARD = registerItem("comet_shard", new Item(new Item.Settings()));
+    public static final Item STAR_PIECE = registerItem("star_piece", new Item(new Item.Settings()));
+    public static final Item STAR_DUST = registerItem("star_dust", new Item(new Item.Settings()));
+    public static final Item WITHER_SKULL_FRAGMENT = registerItem("wither_skull_fragment", new Item(new Item.Settings()));
+    public static final Item RED_SHARD = registerItem("red_shard", new Item(new Item.Settings()));
+    public static final Item BLUE_SHARD = registerItem("blue_shard", new Item(new Item.Settings()));
+    public static final Item GREEN_SHARD = registerItem("green_shard", new Item(new Item.Settings()));
+    public static final Item YELLOW_SHARD = registerItem("yellow_shard", new Item(new Item.Settings()));
+    public static final Item PRETTY_FEATHER = registerItem("pretty_feather", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(GroveExtras.MOD_ID, name), item);
@@ -23,7 +42,7 @@ public class ModItems {
         GroveExtras.LOGGER.info("Registering Mod Items for " + GroveExtras.MOD_ID);
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
-            fabricItemGroupEntries.add(KEY_VOTE);
+            fabricItemGroupEntries.add(WITHER_SKULL_FRAGMENT);
         });
     }
 }
