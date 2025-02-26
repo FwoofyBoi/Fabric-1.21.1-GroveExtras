41b731eeff5d255bc00d7a12d84722653a9ef39a
FwoofyBoi <alexahicks18@gmail.com>
2025-02-26 16:52:27 -0600

    fixed hitbox issues

--- src/main/java/net/fwoofyboi/groveextras/block/custom/GravestoneBlock.java
+++ src/main/java/net/fwoofyboi/groveextras/block/custom/GravestoneBlock.java
@@ -8,6 +8,7 @@ import net.minecraft.block.ShapeContext;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import org.jetbrains.annotations.NotNull;
@@ -16,7 +17,9 @@ import org.jetbrains.annotations.Nullable;
 public class GravestoneBlock extends HorizontalFacingBlock {
 
     public static final MapCodec<GravestoneBlock> CODEC = createCodec(GravestoneBlock::new);
-    private static final VoxelShape SHAPE =
+    private static final VoxelShape X_SHAPE =
+            Block.createCuboidShape(6.5, 0.0, 2.0,9.5, 14.0, 14.0);
+    private static final VoxelShape Z_SHAPE =
             Block.createCuboidShape(2.0, 0.0, 6.5,14.0, 14.0, 9.5);
 
     public GravestoneBlock(Settings settings) {
@@ -25,7 +28,8 @@ public class GravestoneBlock extends HorizontalFacingBlock {
 
     @Override
     protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return SHAPE;
+        Direction direction = state.get(FACING);
+        return direction.getAxis() == Direction.Axis.X ? X_SHAPE : Z_SHAPE;
     }
 
     @Override
