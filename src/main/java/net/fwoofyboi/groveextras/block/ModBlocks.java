cfdb2f7990c1503e7df16ddbca386cd49552f4f0
FwoofyBoi <alexahicks18@gmail.com>
2025-03-27 13:50:37 -0500

    removed tool dependancies for mines blocks, changed strength to 0.5f from 1f

--- src/main/java/net/fwoofyboi/groveextras/block/ModBlocks.java
+++ src/main/java/net/fwoofyboi/groveextras/block/ModBlocks.java
@@ -47,11 +47,11 @@ public class ModBlocks {
             new Block(AbstractBlock.Settings.create().strength(1f).requiresTool().sounds(BlockSoundGroup.STONE)));
 
     public static final Block MINES_FOSSILISED_SAND = registerBlock("mines_fossilised_sand",
-            new Block(AbstractBlock.Settings.create().strength(1f).requiresTool().sounds(BlockSoundGroup.SAND)));
+            new Block(AbstractBlock.Settings.create().strength(0.5f).requiresTool().sounds(BlockSoundGroup.SAND)));
     public static final Block MINES_FOSSILISED_STONE = registerBlock("mines_fossilised_stone",
-            new Block(AbstractBlock.Settings.create().strength(1f).requiresTool().sounds(BlockSoundGroup.STONE)));
+            new Block(AbstractBlock.Settings.create().strength(0.5f).requiresTool().sounds(BlockSoundGroup.STONE)));
     public static final Block MINES_FOSSILISED_DEEPSLATE = registerBlock("mines_fossilised_deepslate",
-            new Block(AbstractBlock.Settings.create().strength(1f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+            new Block(AbstractBlock.Settings.create().strength(0.5f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
 
     public static final Block BUDDING_CRYSTAL_BLUE = registerBlock("budding_crystal_blue",
             new Block(AbstractBlock.Settings.create().strength(1.5f).requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
